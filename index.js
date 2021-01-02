4aa35383722feeee38cb946aea0c4bf315bdd8ad
Tong Pham <thanhtong89@gmail.com>
2021-01-02 22:57:07 +0000

    Added support for score reset and getting previous score

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const DATABASE = "./private/scores.json";
+const DATABASE_PREV = "./private/scores_prev.json";
+
 const MAX_SCORE_ENTRIES = 3;
 var fs = require('fs');
 
@@ -11,16 +13,35 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'private')));
 const mutex = new async_mutex.Mutex();
 
-app.get('/scores', (req, res) => {
+var nextResetDate = getNextResetDate();
+
+app.get('/reset-date', (req, res) => {
+    const answer = {
+        "nextResetDate" : nextResetDate,
+    }
+    res.setHeader('Content-Type', 'application/json');
+    res.json(answer);
+});
+
+app.get('/scores/prev', (req, res) => {
     var scoreTally = loadScoresFromDisk(DATABASE);
     if (scoreTally === null) {
-        scoreTally = [];
+        scoreTally = {"top_scores" : {}};
     }
     res.setHeader('Content-Type', 'application/json');
     res.json(scoreTally);
 });
 
-// expected format: 
+app.get('/scores', (req, res) => {
+    var scoreTally = loadScoresFromDisk(DATABASE);
+    if (scoreTally === null) {
+        scoreTally = {"top_scores" : {}};
+    }
+    res.setHeader('Content-Type', 'application/json');
+    res.json(scoreTally);
+});
+
+// expected format:
 // {name: "name", score: score}
 // we will store this into local JSON
 // Schema is : {top_scores: {name : {score: score, attempts: attempts}, ...}} . Max ten entries.
@@ -38,7 +59,7 @@ app.post('/scores', (req, res) => {
 });
 
 function handlePost(req, res) {
-    var scoreTally = loadScoresFromDisk(DATABASE); 
+    var scoreTally = loadScoresFromDisk(DATABASE);
     if (scoreTally === null) {
         scoreTally = {
             "top_scores": {},
@@ -49,12 +70,12 @@ function handlePost(req, res) {
     // ignore this post if name is empty or score is zero
     if (newScoreEntry.score === 0 || newScoreEntry.name.length === 0) {
         console.log("Ignoring this score");
-        res.json(scoreTally); 
+        res.json(scoreTally);
         return;
     }
     var newScoreTally = updateNewScore(newScoreEntry, scoreTally.top_scores);
     console.log("New score table", newScoreTally);
-    saveScoresToDisk(DATABASE, newScoreTally); 
+    saveScoresToDisk(DATABASE, newScoreTally);
     res.json(newScoreTally);
 }
 
@@ -78,17 +99,17 @@ function updateNewScore(newEntry, topScores) {
     });
     newScoreList.sort((a,b) => {return b["score"] - a["score"]});
     newScoreList = newScoreList.slice(0, MAX_SCORE_ENTRIES);
-    
+
     var result = {"top_scores" : {}};
     newScoreList.forEach(entry => {
         result.top_scores[entry.name] = {score: topScores[entry.name].score, attempts: topScores[entry.name].attempts}
     });
-    return result; 
+    return result;
 }
 
 function loadScoresFromDisk(path) {
     var raw;
-    try { 
+    try {
         raw = fs.readFileSync(path);
     } catch(err) {
         console.log(`Got error reading file ${path}: ${err}`);
@@ -100,5 +121,47 @@ function loadScoresFromDisk(path) {
 function saveScoresToDisk(path, data) {
     fs.writeFileSync(path, JSON.stringify(data));
 }
+
+// obtains the time to next score reset
+function getNextResetDate() {
+    const now = new Date();
+    const next = new Date(now.getFullYear(), (now.getMonth() + 1) % 12, 1);
+//    const next = new Date(now.getTime() + 1*10000);
+    return next;
+}
+
+function resetScore(path) {
+    mutex.acquire()
+        .then(release => {
+            try {
+                currentScores = loadScoresFromDisk(DATABASE);
+                saveScoresToDisk(DATABASE_PREV, currentScores);
+                // purge the new score
+                try {
+                    fs.unlinkSync(DATABASE);
+                }
+                catch (error) {
+                    console.log("Database is already deleted");
+                }
+                console.log("Purged old database");
+            } finally {
+                release();
+            }
+         });
+}
+
+function watchAndResetScores() {
+    const now = new Date();
+    if (now > nextResetDate) {
+        console.log("Will reset scores...");
+        resetScore();
+        nextResetDate = getNextResetDate();
+        console.log("New reset date = ", nextResetDate);
+    } else {
+        console.log("Checking if need to reset ");
+    }
+}
+
+setInterval(watchAndResetScores, 5000);
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
